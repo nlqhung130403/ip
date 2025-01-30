fba2c118ee6e8e3534dc92d99d188c06697b411d
nlqhung130403 <e1157278@u.nus.edu>
2025-01-30 16:44:55 +0800

    Change names and greetings

---
+++ src/main/java/Ferb.java
@@ -0,0 +1,6 @@
+public class Ferb {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Ferb\nWhat can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
