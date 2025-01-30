b1ddefa1dd7415c287e9367a44ea1a46442f9f98
nlqhung130403 <e1157278@u.nus.edu>
2025-01-30 20:36:01 +0800

    implement echo

--- src/main/java/Ferb.java
+++ src/main/java/Ferb.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
 public class Ferb {
+    private static String INDENT = "    ";
+
+    private static void run(){
+        Scanner scanner = new Scanner(System.in);
+
+        //Create a loop for inputs and terminate when bye
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                System.out.println(Ferb.INDENT + "Bye. Hope to see you again soon!");
+                scanner.close();
+                break;
+            }
+            System.out.println(Ferb.INDENT + command);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Ferb\nWhat can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Hello! I'm Ferb\nWhat can I do for you?\n");
+        Ferb.run();
     }
 }
