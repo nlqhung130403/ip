7067a75469a2373e67a51edde1e9ac363b81c3e3
nlqhung130403 <e1157278@u.nus.edu>
2025-02-03 00:36:16 +0800

    Read and Write to file

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -4,7 +4,11 @@ public class Event extends Task{
     private String endDate;
 
     public Event(String description, String startDate, String endDate) {
-        super(description);
+        this(false, description, startDate, endDate);
+    }
+
+    public Event(boolean isDone, String description, String startDate, String endDate) {
+        super(isDone, description);
         this.startDate = startDate;
         this.endDate = endDate;
     }
@@ -13,5 +17,11 @@ public class Event extends Task{
     public String toString() {
         return "[E]" + this.displayDone() + this.taskDescription()
                 + " (from: " + this.startDate + " to: " + this.endDate + ")";
+    }
+
+    @Override
+    public String toSave() {
+        String isDone = this.isDone() ? "1" : "0";
+        return "E|" + super.toSave() + "|" + this.startDate + "|" + this.endDate;
     }
 }
--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -3,9 +3,18 @@ public class ToDo extends Task{
         super(description);
     }
 
+    public ToDo(boolean isDone, String description) {
+        super(isDone, description);
+    }
+
     @Override
     public String toString() {
         return "[T]" + this.displayDone() + this.taskDescription();
+    }
+
+    @Override
+    public String toSave() {
+        return "T|" + super.toSave();
     }
 
 }
